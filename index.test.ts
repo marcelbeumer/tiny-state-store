ffc80d417f857d0f6bad45a7d7c378bff3a7dbea
Marcel Beumer <marcel@marcelbeumer.com>
2019-06-15 15:58:46 +0200

    Basic impl

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,53 @@
-// import sinon from 'sinon';
-// import assert from 'assert';
+import { assert } from 'chai';
+import { JsonObject } from 'type-fest';
+import createStore, { MergeFn } from '.';
 
-it('test', () => {});
+interface TestState {
+  title: string | null;
+  duration: number | null;
+  metadata: JsonObject;
+}
+
+const createInitialState = (): TestState => ({
+  title: null,
+  duration: null,
+  metadata: {}
+});
+
+it('getState returns reference to current state object (no copy)', () => {
+  const store = createStore<TestState>(createInitialState());
+  const stateA = store.getState();
+  const stateB = store.getState();
+  assert.equal(stateA, stateB);
+});
+
+it('setState creates a new object', () => {
+  const store = createStore<TestState>(createInitialState());
+  const stateA = store.getState();
+  store.setState({});
+  const stateB = store.getState();
+  assert.notEqual(stateA, stateB);
+});
+
+it('setState makes a shallow copy', () => {
+  const store = createStore<TestState>(createInitialState());
+  store.setState({ duration: 100, metadata: { author: 'foo' } });
+  const stateA = store.getState();
+  store.setState({ duration: 100, metadata: { author: 'foo' } });
+  const stateB = store.getState();
+
+  assert.notEqual(stateA, stateB);
+  assert.deepEqual(stateA, stateB);
+  assert.notEqual(stateA.metadata, stateB.metadata);
+});
+
+it('supports passing custom merge fn', () => {
+  const mergeFn: MergeFn<TestState> = (a, b) => ({ ...a, ...b, duration: 999 });
+  const store = createStore<TestState>(createInitialState(), { mergeFn: mergeFn });
+  store.setState({ title: 'hello' });
+  assert.deepEqual(store.getState(), {
+    ...createInitialState(),
+    title: 'hello',
+    duration: 999
+  });
+});
--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
-export default () => {
-  return null;
+import { ReadonlyDeep } from 'type-fest';
+
+export interface Store<State> {
+  getState(): ReadonlyDeep<State>;
+  setState(update: Partial<State>): void;
+}
+
+export type MergeFn<State> = (a: State, b: Partial<State>) => State;
+
+export interface StoreOptions<State> {
+  mergeFn?: MergeFn<State>;
+}
+
+const createStore = <State>(initialState: State, options?: StoreOptions<State>): Store<State> => {
+  let state: State = initialState;
+  const merge: MergeFn<State> = (options && options.mergeFn) || ((a, b) => ({ ...a, ...b }));
+  return {
+    getState: () => state as ReadonlyDeep<State>,
+    setState(update) {
+      state = merge(state, update);
+    }
+  };
 };
+
+export default createStore;

47468317c17b3227671d83e75efe86681ac49bb0
Marcel Beumer <marcel@marcelbeumer.com>
2019-06-15 23:12:18 +0200

    OnChange param and better merge fn

--- index.ts
+++ index.ts
@@ -1,23 +1,35 @@
-import { ReadonlyDeep } from 'type-fest';
-
-export interface Store<State> {
-  getState(): ReadonlyDeep<State>;
-  setState(update: Partial<State>): void;
+export interface State {
+  [index: string]: any;
 }
 
-export type MergeFn<State> = (a: State, b: Partial<State>) => State;
-
-export interface StoreOptions<State> {
-  mergeFn?: MergeFn<State>;
+export interface Store<T extends State, U = Partial<T>> {
+  getState(): T;
+  setState(update: U): void;
 }
 
-const createStore = <State>(initialState: State, options?: StoreOptions<State>): Store<State> => {
-  let state: State = initialState;
-  const merge: MergeFn<State> = (options && options.mergeFn) || ((a, b) => ({ ...a, ...b }));
+export type HasChanged = boolean;
+export type MergeFn<T extends State, U = Partial<T>> = (a: T, b: U) => [T, HasChanged];
+export type OnChangeFn<T extends State> = (state: T, prevState: T) => void;
+
+export function shallowMerge<T extends State>(a: T, b: Partial<T>): [T, HasChanged] {
+  const changed = Object.keys(b).some(name => a[name] !== b[name]);
+  return [changed ? { ...a, ...b } : a, changed];
+}
+
+const createStore = <T extends State, U = Partial<T>>(
+  initialState: T,
+  onChange?: OnChangeFn<T> | null,
+  mergeFn?: MergeFn<T, U> | null
+): Store<T, U> => {
+  let state: T = initialState;
+  const merge: MergeFn<T, U> = mergeFn || ((a, b) => shallowMerge<T>(a, b));
   return {
-    getState: () => state as ReadonlyDeep<State>,
+    getState: () => state,
     setState(update) {
-      state = merge(state, update);
+      const prevState = state;
+      const [newState, hasChanged] = merge(state, update);
+      state = newState;
+      if (hasChanged && onChange) onChange(newState, prevState);
     }
   };
 };

0cc845a61b2a320d25f6d27c49d590bf343a9185
Marcel Beumer <marcel@marcelbeumer.com>
2019-06-16 04:31:40 +0200

    Rename things and add subscribe/unsubscribe

--- index.ts
+++ index.ts
@@ -5,10 +5,14 @@ export interface State {
 export interface Store<T extends State, U = Partial<T>> {
   getState(): T;
   setState(update: U): void;
+  subscribe(listener: Listener<T>): void;
+  unsubscribe(listener: Listener<T>): void;
+  setReducer(reducer: Reducer<T, U>): void;
+  getReducer(): Reducer<T, U>;
 }
 
-export type MergeFn<T extends State, U = Partial<T>> = (a: T, b: U) => T;
-export type OnChangeFn<T extends State> = (state: T, prevState: T) => void;
+export type Reducer<T extends State, U = Partial<T>> = (a: T, b: U) => T;
+export type Listener<T extends State> = (state: T, prevState: T) => void;
 
 export function shallowMerge<T extends State>(a: T, b: Partial<T>): T {
   const changed = Object.keys(b).some(name => a[name] !== b[name]);
@@ -17,17 +21,34 @@ export function shallowMerge<T extends State>(a: T, b: Partial<T>): T {
 
 const createStore = <T extends State, U = Partial<T>>(
   initialState: T,
-  onChange?: OnChangeFn<T> | null,
-  mergeFn?: MergeFn<T, U> | null
+  listener?: Listener<T> | null,
+  reducer?: Reducer<T, U> | null
 ): Store<T, U> => {
   let state: T = initialState;
-  const merge: MergeFn<T, U> = mergeFn || ((a, b) => shallowMerge<T>(a, b));
+  const listeners: Listener<T>[] = [];
+  let reduce: Reducer<T, U> = reducer || ((a, b) => shallowMerge<T>(a, b));
+  if (listener) listeners.push(listener);
   return {
     getState: () => state,
     setState(update) {
       const prevState = state;
-      state = merge(state, update);
-      if (state !== prevState && onChange) onChange(state, prevState);
+      state = reduce(state, update);
+      if (state !== prevState) {
+        listeners.forEach(handler => handler(state, prevState));
+      }
+    },
+    getReducer() {
+      return reduce;
+    },
+    setReducer(reducer) {
+      reduce = reducer;
+    },
+    subscribe(listener) {
+      listeners.push(listener);
+    },
+    unsubscribe(listener) {
+      const idx = listeners.indexOf(listener);
+      if (idx !== -1) listeners.splice(idx, 1);
     }
   };
 };
